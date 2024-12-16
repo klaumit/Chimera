ae41a3de2d4acee1571c18548943780b8a4fd04b
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-12-16 18:09:45 +0100

    Renaming folders

---
+++ Compilers/LSIC/setenv.bat
@@ -0,0 +1,8 @@
+REM @echo off
+
+set LSDIR=X:\Compilers\LSIC
+set PATH=%LSDIR%\bin;%PATH%
+set LSC_INC=%LSDIR%\include
+set LSC_LIB=%LSDIR%\lib
+set MAKEDEFAULT=%LSDIR%\bin\makedef
+
---
+++ Compilers/SHC/setenv.bat
@@ -0,0 +1,8 @@
+REM @echo off
+
+set TCDIR=X:\Compilers\SHC
+set PATH=%TCDIR%\bin;%PATH%
+set SHC_INC=%TCDIR%\include
+set SHC_LIB=%TCDIR%\bin
+set SHC_TMP=%TCDIR%
+
