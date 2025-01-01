43f4563c1029e36771bc3af4e886215590ec2f38
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-01-01 04:31:22 +0100

    Some tests

--- Sources/Operation/C/Op.c
+++ Sources/Operation/C/Op.c
@@ -4,8 +4,12 @@
 void main(void)
 {
     volatile signed int a = 13;
-    volatile unsigned int b = 4;
-    print_char(a << b);
+    volatile unsigned int b = 22;
+    print_char(a + b);
+    print_char(a - b);
+    print_char(a * b);
+    print_char(b / b);
+    print_char(b % b);
 }
 
 void print_char(short c)
