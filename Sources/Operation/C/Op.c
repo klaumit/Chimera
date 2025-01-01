682de360c87be7d2c413ee27fe0bd2b51f66192a
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-01-01 03:43:15 +0100

    Getting raw binary

--- Sources/Operation/C/Op.c
+++ Sources/Operation/C/Op.c
@@ -1,62 +1,14 @@
 #include "libc.h"
 #include "op.h"
 
-void print_char(short arg)
-{
-    /* TODO */
-}
-
 void main(void)
 {
-    volatile signed char x1 = 3;
-    volatile unsigned char x2 = 3;
-    volatile signed short x3 = 3;
-    volatile unsigned short x4 = 3;
-    volatile signed int x5 = 3;
-    volatile unsigned int x6 = 3;
-    volatile signed long x7 = 3;
-    volatile unsigned long x8 = 3;
-    volatile float x9 = 3;
-    volatile double x10 = 3;
-    volatile long double x11 = 3;
-
-    char x12[] = "hello";
-    char x13[5] = "hello";
-
-    int c[10] = {1, 2, 3};
-    int d[] = {1, 2, 3};
-    int e[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int f[][3] = {{1}, {2, 3}, {4, 5, 6}};
-
-    volatile unsigned short a = 31484;
-    volatile unsigned short b = 12393;
-
-    print_char(c[0] + c[1]);
-    print_char(d[1] * d[0]);
-    print_char(e[1][2] - e[0][0]);
-    print_char(f[1][1]);
-
-    print_char(a + b);
-    print_char(a - b);
+    volatile signed int a = 13;
+    volatile unsigned int b = 4;
     print_char(a << b);
-    print_char(a >> b);
-    print_char(a < b);
-    print_char(a > b);
-    print_char(a <= b);
-    print_char(a >= b);
-    print_char(a == b);
-    print_char(a != b);
-    print_char(++b);
-    print_char(--b);
-    print_char(a & b);
-    print_char(a ^ b);
-    print_char(a * b);
-    print_char(a / b);
-    print_char(a % b);
-    print_char(~a);
-    print_char(!b);
-    print_char(a | b);
-    print_char(a && b);
-    print_char(a || b);
-    print_char(a == b ? 1 : 0);
+}
+
+void print_char(short c)
+{
+    /* TODO ??? */
 }
--- Sources/Operation/raw_build.bat
+++ Sources/Operation/raw_build.bat
@@ -1,24 +1,34 @@
 @echo off
 
+echo ::: Setting up environment :::
 call ..\..\setraw.bat
 set PATH=%LSBIN%;%LSTOO%;%PATH%
 
+echo ::: Deleting old binaries :::
 rd /s /q bin
 rd /s /q obj
 mkdir bin
 mkdir obj
 
+echo ::: Compiling to text :::
+lcc86 -SC ^
+ -ms -a -v0 -O -c -cs -Ih ^
+ -I%PVDIR%\com_h -I%PVDIR%\com_lib\h ^
+ -oobj\op.txt c/op.c
+
+echo ::: Compiling to object :::
 lcc86 ^
- -mp -a -v0 -O -c -cs -Ih ^
+ -ms -a -v0 -O -c -cs -Ih ^
  -I%PVDIR%\com_h -I%PVDIR%\com_lib\h ^
  -oobj\op.obj c/op.c
 
+echo ::: Linking to hex :::
 lld ^
  -Fh -TDATA 19000 -TSYS_DATA 18400 -TSYS_EXTDATA 18000 -TXSTACK 1E000 ^
- -TFAR_BSS 08000 -M -TTEXT 80000 -oobj\op.hex ^
- %PVDIR%\com_obj\p\start.obj %PVDIR%\com_obj\p\libdata.obj obj\op.obj ^
- %PVDIR%\com_lib\p\libc.lib -L%LSC_LIB% -lp/doslib
+ -TFAR_BSS 08000 -M -TTEXT 80000 -oobj\op.hex obj\op.obj -L%LSC_LIB%
 
+echo ::: Converting to binary :::
 hex2bin obj\op.hex bin\op_x86.bin
 
+echo ::: Done :::
 
