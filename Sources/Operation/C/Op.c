54881812973bbca76c08a447ec18eb14257d635c
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-01-02 21:42:58 +0100

    Generating dos-compatible with one of both

--- Sources/Operation/C/Op.c
+++ Sources/Operation/C/Op.c
@@ -6,9 +6,36 @@ void main(void)
     volatile signed int a = 0x13;
     volatile unsigned int b = 0x22;
     print_char(a - b);
+    print_char(a - b);
+    print_nl();
+    print_char(a - b);
+    print_nl();
+    exit_dos();
+}
+
+void exit_dos(void)
+{
+#ifdef _M86
+    nf_exit();
+#elif _MH3
+    Vagina1
+#endif
+}
+
+void print_nl(void)
+{
+#ifdef _M86
+    nf_nl();
+#elif _MH3
+    Vagina2
+#endif
 }
 
 void print_char(short c)
 {
-    /* TODO ??? */
+#ifdef _M86
+    nf_char(c);
+#elif _MH3
+    Vagina3
+#endif
 }
--- Sources/Operation/H/Op.h
+++ Sources/Operation/H/Op.h
@@ -3,6 +3,21 @@
 #define EXP_DEF
 #include "define.h"
 
+void exit_dos(void);
 void print_char(short c);
+void print_nl(void);
+
+#ifdef _M86
+
+#define nf_exit() _asm_nf_exit("\n mov ax, 4C00h \n int 21h \n")
+void _asm_nf_exit();
+
+#define nf_char(c) _asm_nf_char("\n mov dl, al \n mov ah, 02h \n int 21h \n", c)
+void _asm_nf_char(unsigned char *, unsigned char);
+
+#define nf_nl() _asm_nf_nl("\n mov ah, 02h \n mov dl, 0Dh \n int 21h \n mov dl, 0Ah \n int 21h \n")
+void _asm_nf_nl();
+
+#endif
 
 #endif
