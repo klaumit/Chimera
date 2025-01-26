4066da5d87107e70665229da0645f333984bddf0
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-01-26 03:48:23 +0100

    Adding some loops

--- Sources/Operation/C/Op.c
+++ Sources/Operation/C/Op.c
@@ -3,8 +3,10 @@
 
 void main(void)
 {
-    volatile signed int a = 0x4A;
+	volatile signed int a = 0x4A;
     volatile unsigned int b = 0x12;
+	volatile int xa[4] = {0x2B,35,75,100};
+	volatile int xb[2][3] = { {1,4,2}, {3,6,8 } };
     print_char(a - b); /* 8 */
 
     a = 0x1A;
@@ -64,6 +66,53 @@ void main(void)
 
     print_char((!(a > b)) ? 'Y' : 'N');
 
+	if (a >= b){
+		print_char('1');
+	} else {
+		print_char('2');
+	}
+	
+	a = 0x91;
+	switch (a) {
+		case 0x91:
+			print_char('S');
+			break;
+		default:
+			print_char('F');
+			break;
+	}
+	
+	a = 3;
+	while (a > 0) {
+		print_char('0' + (a--));
+	}
+	
+	a = 4;
+	do {
+		print_char('A' + (a+=2));
+	} while (a < 11);
+	
+	a = 0x1234;
+	b = 0;
+	
+	while (a) {
+		b = b * 10 + a % 10;
+		a /= 10;
+	}
+	print_char((b >> 8) + 0x30);
+	
+	a = 4;
+	for (a = 0; a < 8; a+=3)
+		print_char('0' + a);
+	
+	print_char(xa[1] + 6 + 23 + 5);
+	xa[1] = 0x33;
+	print_char(xa[1] + xa[0] + 14);
+	
+	print_char('0'+sizeof(xa));
+	
+	print_char(xb[0][2]+'a');
+	
     exit_dos();
 }
 
